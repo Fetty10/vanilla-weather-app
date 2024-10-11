4eae0b6476f73e4b84befd00872e9b1018b28980
Faith Osabutey <osabuteyfaith@gmail.com>
2024-10-11 03:56:59 +0100

    Added search functionality using weather api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-function searchCity(event) {
+function getWeatherTempterature(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "o74291bedbb48f3c3et5a5a6ad40853a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeatherTempterature);
+}
+
+function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", handleSearch);
